814562dc3c949ef8e37dec7a6d5e22c71a7206fe
Alphawastaken <76787048+Alphawastaken@users.noreply.github.com>
2021-02-20 17:23:47 +0200

    Create Massban_example.py

---
+++ Massban_example.py
@@ -0,0 +1,42 @@
+# WARNING!
+# DO NOT use this script for malicious purposes!
+# Author:Alpha®#0001
+
+import discord
+import asyncio
+from discord.ext import commands
+
+token = 'YOUR BOTS TOKEN'
+
+client = commands.Bot(command_prefix = ';')
+
+@client.event
+async def on_ready():
+    print('Online')
+    
+
+
+
+
+# Mass Banning Users that have a specific role
+@client.command()
+@commands.is_owner()
+async def massroleban(ctx,rolename: discord.Role):
+    for role in rolename.members:
+        try:
+            await role.ban(reason="Any reason", delete_message_days=7)
+            await ctx.send(f"All users with {rolename} have been banned")
+        except:
+            pass
+
+#Mass Banning all discord server Users
+@client.command()
+@commands.is_owner()
+async def massban(ctx):
+    for user in ctx.guild.members:
+        try:
+            await user.ban(reason="Any reason", delete_message_days=7)
+            await ctx.send(f"All users have been banned")
+        except:
+            pass
+client.run(token, bot = True)
